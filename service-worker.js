e8604e198ba0c849dc947e3ef5017dc65ed132f0
jfotech <jfoaitech@gmail.com>
2026-04-28 21:28:27 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('jfo-cache-v1').then((cache) => {
+    caches.open("jfo-cache").then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
